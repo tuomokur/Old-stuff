f07e6548002773871d4471b8c08faa544255f8aa
Tuomo Kurikka <32066431+tuomokur@users.noreply.github.com>
2018-03-01 12:53:15 +0200

    Create Alku.java
    Yksinkertainen geometriapeli

---
+++ UI_Ohjelmointi_Javalla/Alku.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+public class Alku {
+	//main
+	public static void main (String arg[]){
+		
+		//main framen alustus
+		AloitusIkkuna fw = new AloitusIkkuna();
+		fw.setLocationRelativeTo(null);
+		fw.setVisible(true);
+		fw.setResizable(false);
+		fw.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+
+	}
+
+}
